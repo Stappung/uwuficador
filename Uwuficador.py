c578fc0020128c5502bcb38bd368d2c7f8ab519f
v4rgas <66626747+v4rgas@users.noreply.github.com>
2020-09-07 12:27:48 -0300

    pequeños cambios al main

--- Uwuficador.py
+++ Uwuficador.py
@@ -1,50 +1,49 @@
 import random 
 
-def main():
-    
+def uwuficador(letra):
+    uwutxt = letra
+    if letra == " ":
+        filler = [" :3 "," ♥ "," ✌ "," ᵘʷᵘ "," UwU "," Uwu "," uwU "," ÚwÚ "," uwu "," ☆w☆ "," ✧w✧ "," ♥w♥ "," uw ︠u "," (uwu) "]
+        fillerfrases = [" **se sonroja** "," **muerde el cojin** "," **sonrie** "," **ʕ •ᴥ•ʔ** "] 
+        if prob(25):
+            n = random.randint(0, len(filler)-1)
+            uwutxt = filler[n] 
+        elif prob(10):
+            n = random.randint(0, len(fillerfrases)-1)
+            uwutxt = fillerfrases[n] 
+    else:
+        alfabeto = {
+            "a": "u",
+            "h": "",
+            "g": "w",
+            "y": "i",
+            "o": "owo",
+            "r": "g",
+            ".": "ღ"
+        }
+        uwutxt = alfabeto.get(letra, letra)
+    return uwutxt
 
-    def uwuficador(letra):
-        uwutxt = letra
-        if letra == " ":
-            filler = [" :3 "," ♥ "," ✌ "," ᵘʷᵘ "," UwU "," Uwu "," uwU "," ÚwÚ "," uwu "," ☆w☆ "," ✧w✧ "," ♥w♥ "," uw ︠u "," (uwu) "]
-            fillerfrases = [" **se sonroja** "," **muerde el cojin** "," **sonrie** "," **ʕ •ᴥ•ʔ** "] 
-            if prob(25):
-                n = random.randint(0, len(filler)-1)
-                uwutxt = filler[n] 
-            elif prob(10):
-                n = random.randint(0, len(fillerfrases)-1)
-                uwutxt = fillerfrases[n] 
-        else:
-            alfabeto = {
-                "a": "u",
-                "h": "",
-                "g": "w",
-                "y": "i",
-                "o": "owo",
-                "r": "g",
-                ".": "ღ"
-            }
-            uwutxt = alfabeto.get(letra, letra)
-        return uwutxt
+def prob(proba):
+    numero = random.randint(0,99)
+#    print(numero)
+    if numero <= proba:
+        return True
+    else:
+        return False
 
-    def prob(proba):
-        numero = random.randint(0,99)
-    #    print(numero)
-        if numero <= proba:
-            return True
-        else:
-            return False
-
-    #Lo mejor del mundo viene a continuación
-    txt = input()
-    txtFinal = ''
-    uwu = int(input("Que tan UwW? "))
+#Lo mejor del mundo viene a continuación
+txt = input()
+txtFinal = ''
+uwuness = int(input("Que tan UwW? 0 - 99 "))
+if uwuness>99:
+    print('debe ser menor a 99 y mayor que 0')
+else:
 
     for letra in txt:
-        if prob(uwu):
+        if prob(uwuness):
             txtFinal += uwuficador(letra)
         else:
             txtFinal += letra
     print(txtFinal,"uwu")
 
-main()
---
+++ uwufytxt.py
@@ -0,0 +1,47 @@
+import random 
+
+
+def uwuficador(letra):
+    uwutxt = letra
+    if letra == " ":
+        filler = [" :3 "," ♥ "," ✌ "," ᵘʷᵘ "," UwU "," Uwu "," uwU "," ÚwÚ "," uwu "," ☆w☆ "," ✧w✧ "," ♥w♥ "," uw ︠u "," (uwu) "]
+        fillerfrases = [" **se sonroja** "," **muerde el cojin** "," **sonrie** "," **ʕ •ᴥ•ʔ** "] 
+        if prob(25):
+            n = random.randint(0, len(filler)-1)
+            uwutxt = filler[n] 
+        elif prob(10):
+            n = random.randint(0, len(fillerfrases)-1)
+            uwutxt = fillerfrases[n] 
+    else:
+        alfabeto = {
+            "a": "u",
+            "h": "",
+            "g": "w",
+            "y": "i",
+            "o": "owo",
+            "r": "g",
+            ".": "ღ"
+        }
+        uwutxt = alfabeto.get(letra, letra)
+    return uwutxt
+
+def prob(proba):
+    numero = random.randint(0,99)
+#    print(numero)
+    if numero <= proba:
+        return True
+    else:
+        return False
+
+#Lo mejor del mundo viene a continuación
+txt = input()
+txtFinal = ''
+uwu = int(input("Que tan UwW? "))
+
+for letra in txt:
+    if prob(uwu):
+        txtFinal += uwuficador(letra)
+    else:
+        txtFinal += letra
+print(txtFinal,"uwu")
+
